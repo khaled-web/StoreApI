5a641ccbac830644bd194389fbbe99e13f924d9c
Khaled Mohamed Ibrahim <eng.khaled_ok@outlook.com>
2022-02-16 16:40:54 +0200

    filtering by using company, featured

--- controllers/products.js
+++ controllers/products.js
@@ -3,9 +3,7 @@ const product = require('../models/product')
 
 //function for testing-Data
 const getAllProductsStatic = async (req, res) => {
- const products = await product.find({ //basic find_data
-  name: 'accent chair'
- })
+ const products = await product.find(req.query)
  // throw new Error('testing async errors')
  res.status(200).json({
   products,
@@ -13,9 +11,25 @@ const getAllProductsStatic = async (req, res) => {
  })
 }
 
-//function for writing-Data
+//function for reading-Data
 const getAllProducts = async (req, res) => {
- const products = await product.find(req.query);
+ const {
+  featured,
+  company
+ } = req.query
+ const queryObject = {}
+
+ if (featured) {
+  queryObject.featured = featured === 'true' ? true : false;
+ }
+
+ if (company) {
+  queryObject.company = company;
+ }
+
+ console.log(queryObject)
+
+ const products = await product.find(queryObject);
  res.status(200).json({
   products,
   nbHits: products.length
